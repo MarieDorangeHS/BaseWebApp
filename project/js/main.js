18408a0057b902eeba16b40e10839c958c321ee8
Marie Dorange <mdorange@hubspot.com>
2022-10-19 12:31:27 +0200

    Tailoring my page with a search tool and error message

--- project/js/main.js
+++ project/js/main.js
@@ -13,6 +13,9 @@ function getWeather(searchQuery) {
       $(".city").text(data.name);
       $(".temp").text(data.main.temp);
     },
+    error: function (error) {
+      $(".error-message").text("An error occured");
+    },
   });
 }
 
