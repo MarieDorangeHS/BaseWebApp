456860bcad70fca8a38dd279e2b9a1350794370b
Marie Dorange <mdorange@hubspot.com>
2022-11-03 12:59:06 +0100

    simplify web app

--- project/js/main.js
+++ project/js/main.js
@@ -1,52 +1,6 @@
 // $(document).ready(function () {
-// getWeather();
-// })
-
-// $(document).ready(function () {
 // getPost();
 // });
-
-function getWeather(searchQuery) {
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    searchQuery +
-    "&units=metric&appid=" +
-    apiKey;
-
-  $(".city").text("");
-  $(".temp").text("");
-  $(".wind").text("");
-  $(".error-message").text("");
-
-  $.ajax(url, {
-    success: function (data) {
-      $(".city").text(data.name);
-      $(".temp").text(data.main.temp);
-      $(".wind").text(data.wind.speed);
-    },
-    error: function (error) {
-      $(".error-message").text("An error occured!");
-    },
-  });
-}
-
-function showPicture() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append(
-    '<img class="rounded-circle" src="images/high-five.gif"/>'
-  );
-  $("p.reward").html(
-    "High five! You're building your first web app using a simple API!"
-  );
-
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-}
-
-function searchWeather() {
-  var searchQuery = $(".search").val();
-  getWeather(searchQuery);
-}
 
 function handleSignIn() {
   var provider = new firebase.auth.GoogleAuthProvider();
